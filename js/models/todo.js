75a8f431f2b683ffba2ae98d1f128bb4ad7bb74c
mashhouri <a.mashhouri@yahoo.com>
2015-09-25 15:40:59 +0330

    Update todo.js
    change model

--- js/models/todo.js
+++ js/models/todo.js
@@ -1,22 +1,32 @@
-App.Todo = DS.Model.extend({
-  title: DS.attr('string'),
-  isCompleted: DS.attr('boolean')
+Abs.Tasks = DS.Model.extend({
+    title: DS.attr('string'),
+    isDone: DS.attr('boolean')
 });
 
-App.Todo.FIXTURES = [
- {
-   id: 1,
-   title: 'Learn Ember.js',
-   isCompleted: true
- },
- {
-   id: 2,
-   title: 'dot dot dot...',
-   isCompleted: false
- },
- {
-   id: 3,
-   title: 'Profit!',
-   isCompleted: false
- }
+Abs.Tasks.FIXTURES = [
+  {
+      id: 1,
+      title: 'Task 1 description',
+      isDone: true
+  },
+  {
+      id: 2,
+      title: 'Task 2 description',
+      isDone: false
+  },
+  {
+      id: 3,
+      title: 'Task 3 description',
+      isDone: false
+  },
+  {
+      id: 4,
+      title: 'Task 4 description',
+      isDone: true
+  },
+  {
+      id: 5,
+      title: 'Task 5 description',
+      isDone: false
+  }
 ];
