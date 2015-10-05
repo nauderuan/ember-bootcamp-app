e8805ea1ce34bccd07e974f0cc7a3332500b9da3
mashhouri <a.mashhouri@yahoo.com>
2015-10-05 15:33:13 +0330

    standard js array model

--- js/router.js
+++ js/router.js
@@ -2,7 +2,34 @@ Abs.Router.map(function () {
     this.resource('Abs', { path: '/' });
 });
 Abs.AbsRoute = Ember.Route.extend({
-    model: function () {
-        return this.store.find('Tasks');
-    }
+	model: function() {
+	return Abs.Tasks;
+  }
 });
+  Abs.Tasks=[
+  {
+      id: 1,
+      title: 'Task 1 description',
+      isDone: true
+  },
+  {
+      id: 2,
+      title: 'Task 2 description',
+      isDone: false
+  },
+  {
+      id: 3,
+      title: 'Task 3 description',
+      isDone: false
+  },
+  {
+      id: 4,
+      title: 'Task 4 description',
+      isDone: true
+  },
+  {
+      id: 5,
+      title: 'Task 5 description',
+      isDone: false
+  }
+];
