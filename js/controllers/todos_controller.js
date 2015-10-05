22995fcb1bb3b71efaf41330633264689ea87b7c
mashhouri <a.mashhouri@yahoo.com>
2015-10-05 15:37:51 +0330

    remove array controler

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -1,112 +1,54 @@
-
-Abs.TasksController = Ember.ObjectController.extend({
-    actions: {
-        editTasks: function () {
-            this.set('editMode', true);
-        },
-        changeTask: function () {
-            var me = this;
-            Ember.run.later((function () {
-                me.set('editMode', false);
-                if (Ember.isEmpty(me.get('model.title'))) {
-                    me.send('removeTask');
-                } else {
-                    me.get('model').save();
-                }
-            }), 100);
-        },
-        removeTask: function () {
-            var Task = this.get('model');
-            Task.deleteRecord();
-            Task.save();
-        }
-    },
-
-    editMode: false,
-
-    isDone: function (key, value) {
-        var model = this.get('model');
-
-        if (value === undefined) {
-            // property being used as a getter
-            return model.get('isDone');
-        } else {
-            // property being used as  setter
-            model.set('isDone', value);
-            model.save();
-            return value;
-        }
-    }.property('model.isDone')
-});
-
 Abs.AbsController = Ember.ArrayController.extend({
     actions: {
         checkAll: function () {
             var checked = $('#chk-all')[0].checked;
             var store = this.get('store'), model = this.get('model');
-
-            model.forEach(function (task) {
-                task.set('isDone', checked);
-                task.save();
-            });
-
+            model.setEach('isDone', checked)
         },
         createTask: function () {
             // Get the Tasks title set by the "New Tasks" text field
             var title = this.get('newTitle');
             if (!title.trim()) { return; }
-
-            // Create the new Tasks model
-            var Tasks = this.store.createRecord('Tasks', {
+			var newTask = {
                 title: title,
                 isDone: false
-            });
-
+            };
+			this.get('model').pushObject(newTask);
             // Clear the "New Tasks" text field
             this.set('newTitle', '');
 
-            // Save the new model
-            Tasks.save();
         },
 
         removeAllCompleted: function () {
             var model = this.get('model');
-            model.forEach(function (task) {
-                if (task && task.get('isDone')) {
-                    Ember.run.once(this, function () {
-                        task.deleteRecord();
-                        task.save();
-                    });
-                }
-
-            });
-
+			Abs.Tasks.removeObjects(model.filterBy('isDone',true))
         },
 
         allTasks: function () {
-            var me = this, store = me.get('store');
-            me.set('model', store.find('Tasks'));
-            this.removeSelectedClass();
-            $('#allTasksBtn').addClass('selected');
+			this.showTask(null)
         },
         doneTasks: function () {
-            var me = this, store = me.get('store');
-            me.set('model', store.filter('Tasks', function (Tasks) {
-                return Tasks.get('isDone');
-            }));
-            this.removeSelectedClass();
-            $('#doneTasksBtn').addClass('selected');
+         	this.showTask(true)
         },
         activeTasks: function () {
-            var me = this, store = me.get('store');
-            me.set('model', store.filter('Tasks', function (Tasks) {
-                return Tasks.get('isDone') == false;
-            }));
-            this.removeSelectedClass();
-            $('#activeTasksBtn').addClass('selected');
+            
+			this.showTask(false)
         },
-    },
 
+    },
+		showTask: function(isDone){
+		var me = this;
+		            me.removeSelectedClass();
+			me.set('model',Abs.Tasks);
+			if(isDone != null){
+				me.set('model',  me.get('model').filterBy('isDone',isDone));
+				isDone==true?$('#doneTasksBtn').addClass('selected'):$('#activeTasksBtn').addClass('selected');
+			}
+			else
+			{
+				$('#allTasksBtn').addClass('selected');
+			}
+		},
     remainCount: function () {
         return this.filterProperty('isDone', false).get('length');
     }.property('@each.isDone'),
