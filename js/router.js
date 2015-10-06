4eb3ddd874cb580d1f796750fb1d43fb2af1ead4
mashhouri <a.mashhouri@yahoo.com>
2015-10-06 05:42:49 +0330

    remove all model

--- js/router.js
+++ js/router.js
@@ -6,30 +6,3 @@ Abs.AbsRoute = Ember.Route.extend({
 	return Abs.Tasks;
   }
 });
-  Abs.Tasks=[
-  {
-      id: 1,
-      title: 'Task 1 description',
-      isDone: true
-  },
-  {
-      id: 2,
-      title: 'Task 2 description',
-      isDone: false
-  },
-  {
-      id: 3,
-      title: 'Task 3 description',
-      isDone: false
-  },
-  {
-      id: 4,
-      title: 'Task 4 description',
-      isDone: true
-  },
-  {
-      id: 5,
-      title: 'Task 5 description',
-      isDone: false
-  }
-];
